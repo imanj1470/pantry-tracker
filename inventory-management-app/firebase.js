911d2d9ea48cdcbbbd657d33a28d499b9a2190d7
imanj1470 <imanj1470@gmail.com>
2024-07-31 02:44:10 +0100

    adding firebase

---
+++ inventory-management-app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+require("dotenv").config();
+
+
+const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: "pantry-tracker-app-53871.firebaseapp.com",
+  projectId: "pantry-tracker-app-53871",
+  storageBucket: "pantry-tracker-app-53871.appspot.com",
+  messagingSenderId: "533303563078",
+  appId: "1:533303563078:web:e3c22d6937c5f9fbd78b47"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
