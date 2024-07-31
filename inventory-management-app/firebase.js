771e3d30654d93c38b86134670c96caa5e43be99
imanj1470 <imanj1470@gmail.com>
2024-08-01 00:17:08 +0100

    added firestore

--- inventory-management-app/firebase.js
+++ inventory-management-app/firebase.js
@@ -1,12 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getFireStore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
-require("dotenv").config();
 
+
+require("dotenv").config();
 
 const firebaseConfig = {
   apiKey: process.env.FB_API_KEY,
@@ -20,6 +22,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
+const firestore = getFireStore(app)
+export {firestore}
 
 // stoppoint is 13:34
