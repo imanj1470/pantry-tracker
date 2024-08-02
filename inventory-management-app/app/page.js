fc7af98ef4a394a58902b93acf82ec6903e5534c
imanj1470 <imanj1470@gmail.com>
2024-08-02 02:13:02 +0100

    chnges

--- inventory-management-app/app/page.js
+++ inventory-management-app/app/page.js
@@ -7,11 +7,43 @@ import {firestore} from "@/firebase"
 
 import { Stack, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
+import { query } from "firebase/firestore";
 
 export default function None(){
+    const [inventory, setInventory] = useState([])
+    const [open, setOpen] = useState([false])
+    const [itemName, setItemName] = useState([""])
+
+    const updateInventory = async () => {
+        const snapshot = query(collection(firestore, "inventory"))
+        const docs = await getDocs(snapshot)
+        const inventoryList = []
+        docs.forEach( (doc) => {
+            inventoryList.push({
+                name: doc.id,
+                ...doc.data(),
+            })
+        })
+        setInventory(inventoryList)
+        console.log(inventoryList)
+    }
+
+    useEffect(() => {
+        updateInventory()
+    }, [])
+
     return (
         <Box>
             <Typography variant="h1">Inventory management</Typography>
+
+            {
+                inventory.forEach((item) => {
+                    return (<>
+                    {item.name}
+                    {item.count}
+                    </>)
+                })
+            }
         </Box>
     )
 }
--- inventory-management-app/firebase.js
+++ inventory-management-app/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getFireStore} from "firebase/firestore"
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,8 +21,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
 
-const firestore = getFireStore(app)
-export {firestore}
+export { firestore };
 
 // stoppoint is 13:34
