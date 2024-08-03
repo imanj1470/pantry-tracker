e083795c8f018e046042c9e879962013a5ae2a29
imanj1470 <imanj1470@gmail.com>
2024-08-03 03:35:58 +0100

    working. just need to sort out differentiating capitals from non capital data entry

--- inventory-management-app/app/page.js
+++ inventory-management-app/app/page.js
@@ -2,16 +2,13 @@
 
 "use client"
 import Image from "next/image"
-import {useState, useEffect} from "react"
-import {firestore} from "@/firebase"
-import { query, collection, getDocs, getDoc, deleteDoc } from "firebase/firestore";
+import { useState, useEffect } from "react"
+import { firestore } from "@/firebase"
+import { query, collection, doc, getDocs, getDoc, deleteDoc, setDoc } from "firebase/firestore";
 import { Button, Stack, Typography, Modal, TextField, Box } from '@mui/material';
 
 
-
-
-
-export default function None(){
+export default function None() {
     const [inventory, setInventory] = useState([])
     const [open, setOpen] = useState(false)
     const [itemName, setItemName] = useState([""])
@@ -20,7 +17,7 @@ export default function None(){
         const snapshot = query(collection(firestore, "inventory"))
         const docs = await getDocs(snapshot)
         const inventoryList = []
-        docs.forEach( (doc) => {
+        docs.forEach((doc) => {
             inventoryList.push({
                 name: doc.id,
                 ...doc.data(),
@@ -30,31 +27,30 @@ export default function None(){
 
     }
 
-    const removeItem = async(item) => {
+    const removeItem = async (item) => {
         const docRef = doc(collection(firestore, "inventory"), item)
         const docSnap = await getDoc(docRef)
 
-        if (docSnap.exists()){
-            const {quantity} = docSnap.data()
+        if (docSnap.exists()) {
+            const { quantity } = docSnap.data()
 
-            if (quantity === 1){
+            if (quantity === 1) {
                 await deleteDoc(docRef)
-            } else{
-                await setDoc(docRef, {quantity: quantity - 1})
+            } else {
+                await setDoc(docRef, { quantity: quantity - 1 })
             }
         }
         await updateInventory()
     }
 
-    const addItem = async(item) => {
+    const addItem = async (item) => {
         const docRef = doc(collection(firestore, "inventory"), item)
         const docSnap = await getDoc(docRef)
-        
         if (docSnap.exists()) {
-            const {quantity} = docSnap.data()
-            await setDoc(docRef, {quantity: quantity + 1})
-        } else{
-            await setDoc(docRef, {quantity: 1})
+            const { quantity } = docSnap.data()
+            await setDoc(docRef, { quantity: quantity + 1 })
+        } else {
+            await setDoc(docRef, { quantity: 1 })
         }
         await updateInventory()
     }
@@ -70,38 +66,38 @@ export default function None(){
         <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" gap={2} flexDirection="column">
 
             <Modal open={open} onClose={handleClosed}>
-                <Box 
-                position="absolute"
-                top="50%"
-                left = "50%"
-                sx={{transform: "translate(-50%, -50%)",}}
-                width={500}
-                bgcolor="white"
-                border="2px solid #000"
-                boxShadow={24}
-                p={4}
-                display={"flex"}
-                flexDirection="column"
-                gap={3}
+                <Box
+                    position="absolute"
+                    top="50%"
+                    left="50%"
+                    sx={{ transform: "translate(-50%, -50%)", }}
+                    width={500}
+                    bgcolor="white"
+                    border="2px solid #000"
+                    boxShadow={24}
+                    p={4}
+                    display={"flex"}
+                    flexDirection="column"
+                    gap={3}
                 >
                     <Typography variant="h6">Add Item</Typography>
                     <Stack width="100%" direction="row" spacing={2}>
-                        <TextField 
-                        variant="outlined"
-                        fullWidth
-                        value={itemName}
-                        onChange={(e) => {
-                            setItemName(e.target.value)
-                        }}
+                        <TextField
+                            variant="outlined"
+                            fullWidth
+                            value={itemName} 
+                            onChange={(e) => {
+                                setItemName(e.target.value)
+                            }}
                         />
 
                         <Button
-                        variant="outlined"
-                        onClick={() => {
-                            addItem(itemName)
-                            setItemName("")
-                            handleClosed()
-                        }}
+                            variant="outlined"
+                            onClick={() => {
+                                addItem(itemName.charAt(0).toUpperCase() + itemName.slice(1))
+                                setItemName("")
+                                handleClosed()
+                            }}
                         >Add</Button>
                     </Stack>
 
@@ -121,33 +117,34 @@ export default function None(){
                     alignItems="center"
                     justifyContent="center"
                     display="flex"
-                    >
-                        <Typography variant="h2"
+                >
+                    <Typography variant="h2"
                         color="#333">Inventory Items
-                        </Typography>
+                    </Typography>
                 </Box>
-            </Box>
-            <Stack width="300px" height="300px" spacing={2} overflow="auto">
-                {
-                    inventory.map(({name, quantity}) => (
-                        <Box key={name} width="100%"
-                        minHeight="150px" display="flex"
-                        alignItems="center" justifyContent="space-between"
-                        backgroundColor="#f0f0f0" padding={5}>
-                            <Typography variant="h3" color="#333"
-                            textAlign="center">
-                                {name.charAt(0).toUpperCase() + name.slice(1)}
-                            </Typography>
-                            <Typography variant="h3" color="#333"
-                            textAlign="center">{quantity}</Typography>
 
-                            <Button variant="contained" onClick={()=> {
-                                removeItem(name)
-                            }}>Remove</Button>
-                            
-                        </Box>
-                    ))}
-            </Stack>
+                <Stack width="800px" height="300px" spacing={2} overflow="auto">
+                    {
+                        inventory.map(({ name, quantity }) => (
+                            <Box key={name} width="100%"
+                                minHeight="150px" display="flex"
+                                alignItems="center" justifyContent="space-between"
+                                backgroundColor="#f0f0f0" padding={5}>
+                                <Typography variant="h3" color="#333"
+                                    textAlign="center">
+                                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                                </Typography>
+                                <Typography variant="h3" color="#333"
+                                    textAlign="center">{quantity}</Typography>
+
+                                <Button variant="contained" onClick={() => {
+                                    removeItem(name)
+                                }}>Remove</Button>
+
+                            </Box>
+                        ))}
+                </Stack>
+            </Box>
         </Box>
     )
 }
